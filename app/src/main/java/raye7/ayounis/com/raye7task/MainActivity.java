4030203d5f87595d0a7ca65fd8f2429fd9d09b59
ahmed <ahmed.yassin@orchtech.com>
2018-11-26 12:16:32 +0200

    Initial commit

---
+++ app/src/main/java/raye7/ayounis/com/raye7task/MainActivity.java
@@ -0,0 +1,13 @@
+package raye7.ayounis.com.raye7task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
